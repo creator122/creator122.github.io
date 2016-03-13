da7f968ae2931d280bbe7d538e01f742d801906f
creator122 <malpa9000@interia.pl>
2016-03-13 16:32:42 +0100

    Improved the script

--- scripts/script.js
+++ scripts/script.js
@@ -1,100 +1,72 @@
 $(function(){
 	
-	//Determine the offset of elements and whether they are toggled or not
-	
-	var steam = ".sample.steam";
-	var portfolio = ".sample.portfolio";
-	var text = ".text";
-	var elementWidth = $(".square").width();
 	var toggled = false;
-	var noice = {}
 	
+	var elementWidth = $(".square").width();
 	var width = $(window).width()-elementWidth;
-	
-	var position = $(".sample.steam").offset();
-	var offset = position.left;
 	
 	//Check the window height and determine the width of div.
 
-	$(".info.steam").click(function(e){
-		if ($(steam).attr("status") == "off"){
-			$(portfolio).css("visibility","hidden");
-			$(steam).attr("status","on")
-			$( steam ).animate({
-			right: "+="+offset+"px"
+	$(".info").click(function(e){
+		var parent = $(this).parent();
+		var element = $(parent).parent();
+		var children = $("#wrapper").children();
+		var subChildren = $(element).children();
+		var text = subChildren[1];
+		var position = $(element).offset();
+		var offset = position.left;
+		
+		for (child in children){
+			console.log(child[0]);
+		}
+		
+		if ($(element).attr("status") == "off"){
+			$(element).attr("status","on")
+			$(element).animate({
+				right: "+="+offset+"px"
 			}, 1500, function(target) {
-				$(".text.steam").css("display","block");
+				$(text).css("display","block");
 				
 				if ($(window).width()>750){
 					var textBox = {width: 500};
 					width = true;
 					height = false;
+					hide = false;
 				}
 				else{
-					var textBox = {height: 500};
+					var textBox = {height: 200};
 					height = true;
 					width = false;
+					hide = true;
 				}
 				
-				$(".text.steam").animate(textBox, 1500, function(){
-				$(steam).attr("status","on");
-			});
-		});
-	}
-		
-		else{
-			if (height == true && width == false){
-				$( ".text.steam" ).animate({height: 0}, 1500, function() {
-					$(".text.steam").css("display","none");
-					$(steam).attr("status","off");
-					$(portfolio).css("visibility","visible");
-				});
-			}
-			else{
-				$(steam).attr("status","off")
-					$( ".text.steam" ).animate({width: 0}, 1500, function() {
-						$(".text.steam").css("display","none");
-						$(steam).animate({
-							right: "-="+offset+"px"
-					}, 1500, function(){
-						$(steam).attr("status","off");
-						$(portfolio).css("visibility","visible");
-					});
-				});
-			}
-	}
-});	
-	
-	$(".info.portfolio").click(function(){
-		if ($(portfolio).attr("status") == "off"){
-			$(steam).css("visibility","hidden");
-			$( portfolio ).animate({
-			right: "+="+offset+"px"
-			}, 1500, function() {
-				$(".text.portfolio").css("display","block");
-				$(".text.portfolio").animate({
-					target: width
-				}, 1500, function(){
-				$(portfolio).attr("status","on");
+				$(text).animate(textBox, 1500, function(){
+				$(element).attr("status","on");
 			});
 		});
 	}
 		
 		else{
 			
-			$(portfolio).attr("status","off")
-			$( ".text.portfolio" ).animate({
-			width: 0
-			}, 1500, function() {
-				$(".text.portfolio").css("display","none");
-				$(portfolio).animate({
-					right: "-="+offset+"px"
-				}, 1500, function(){
-				$(steam).attr("status","off");
-				$(steam).css("visibility","visible");
-			});
-		});
-	}
+			var offset = position.left;
+			
+			if (height == true && width == false){
+				$( text ).animate({height: 0}, 1500, function() {
+					$(text).css("display","none");
+					$(element).attr("status","off");
+				});
+			}
+			else{
+				$(element).attr("status","off")
+					$( text ).animate({width: 0}, 1500, function() {
+						$(text).css("display","none");
+						$(element).animate({
+							right: -offset
+					}, 1500, function(){
+						$(element).attr("status","off");
+					});
+				});
+			}
+		}
 	});	
-	
 });
